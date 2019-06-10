033ed91481ef1c08fb4e87abf7d12506e4692359
Mateus Braga <braga.mateus16@gmail.com>
2019-06-10 00:26:09 -0300

    resovendo problema CORS

--- comercial-api/src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
+++ comercial-api/src/main/java/com/algaworks/comercial/controller/OportunidadeController.java
@@ -9,6 +9,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ import com.algaworks.comercial.model.Oportunidade;
 import com.algaworks.comercial.repository.OportunidadeRepository;
 
 
-
+@CrossOrigin
 @RestController
 @RequestMapping("/oportunidades")
 public class OportunidadeController {
